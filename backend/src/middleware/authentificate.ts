3ac465d0edf96351467c10946dfe1dd71a0839c9
Andrei Pavlov <andrpav9@gmail.com>
2025-12-08 21:20:20 +0400

    Add errorCode to the authentificate middleware response

--- backend/src/middleware/authentificate.ts
+++ backend/src/middleware/authentificate.ts
@@ -3,17 +3,24 @@ import appAssert from '../utils/appAssert.js';
 import { UNAUTHORIZED } from '../constants/http.js';
 import { verifyToken } from '../utils/jwt.js';
 import mongoose from 'mongoose';
+import AppErrorCode from 'constants/appErrorCode.js';
 
 const authenticate: RequestHandler = (req, res, next) => {
   const accessToken = req.cookies.accessToken as string | undefined;
-  appAssert(accessToken, UNAUTHORIZED, 'Not authorized');
+  appAssert(
+    accessToken,
+    UNAUTHORIZED,
+    'Not authorized',
+    AppErrorCode.InvalidAccessToken
+  );
 
   const { error, payload } = verifyToken(accessToken || '');
 
   appAssert(
     payload,
     UNAUTHORIZED,
-    error === 'jwt expired' ? 'Token expired' : 'Invalid token'
+    error === 'jwt expired' ? 'Token expired' : 'Invalid token',
+    AppErrorCode.InvalidAccessToken
   );
 
   req.userId = new mongoose.Types.ObjectId(payload.userId);
